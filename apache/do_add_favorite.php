3d0af38c8341807bb7efdc41c7f0477f7e20c45b
vdominguezv <92852216+vdominguezv@users.noreply.github.com>
2022-05-31 15:47:47 +0200

    Update do_add_favorite.php

--- apache/do_add_favorite.php
+++ apache/do_add_favorite.php
@@ -5,10 +5,10 @@ $db = mysqli_connect('localhost', 'root','1234','BuscarComidas') or die('Fail');
 $id_usuario = $_SESSION['id_usuario'];
 $id_restaurante = $_POST['id_restaurante'];
 
-echo $id_restaurante;
+
 
 $query = "INSERT INTO tfavorito (restaurante_id,usuario_id) VALUES (".$id_restaurante.",".$id_usuario.")";
-echo $query;
+
 $result = mysqli_query($db,$query) or die('Query Error');
 
 
